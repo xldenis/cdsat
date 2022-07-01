95baf06b572baa8d01536f2ebbf29e268060dfaf
Xavier Denis <xldenis@gmail.com>
2022-06-30 23:37:31 -0700

    Almost finish boolean theory

--- src/concrete.rs
+++ src/concrete.rs
@@ -1,36 +1,117 @@
+use std::collections::{BinaryHeap, HashSet};
+
+use creusot_contracts::proof_assert;
+use priority_queue::PriorityQueue;
+
 use crate::trail::*;
 
 pub struct Solver {
     bool_th: BoolTheory,
 }
 
+enum TheoryState {
+    Sat,
+    Decision(Term, Value),
+    Unknown,
+}
+
 impl Solver {
-    pub fn solver(self, trail: &mut Trail) -> Answer {
+    pub fn solver(&mut self, trail: &mut Trail) -> Answer {
+        // Invariant:
+        // Every theory is coherent up to last_index with the trail
+        // Invariant: trail is sound & has type invariants
         loop {
-        	// Tracks if all theories are satisfied with the trail.
-        	let mut satisfied = (false, false);
+            // Tracks if all theories are satisfied with the trail.
+            let mut states = TheoryState::Unknown;
+            // Invariant:
+            // TheoryState::Sat => Theory_k is Sat for trail
+            // TheoryState::Decision => the decision is acceptable with the current trail
+            // Invariant:
+            // Every theory is coherent up to last_index with the trail
+            // Invariant: trail is sound & has type invariants
             loop {
-            	// If a theory makes progress we reset the satisfaction
-                match self.bool_th.extend(trail) {
-                    ExtendResult::Deduction => satisfied = (false, false),
-                    ExtendResult::Satisfied => satisfied.0 = true,
-                    ExtendResult::Conflict(c) => self.resolve_conflict(trail, c),
+                let trail_len = trail.len();
+                let th_res = self.bool_th.extend(trail);
+                if trail_len != trail.len() {
+                    states = TheoryState::Unknown;
+                };
+
+                match th_res {
+                    ExtendResult::Conflict(c) => {
+                        if trail.level() == 0 {
+                            return Answer::Unsat;
+                        };
+                        states = TheoryState::Unknown;
+                        self.resolve_conflict(trail, c)
+                    }
+                    ExtendResult::Decision(t, v) => states = TheoryState::Decision(t, v),
+                    ExtendResult::Satisfied => states = TheoryState::Sat,
                 }
-                break
+
+                if matches!(states, TheoryState::Decision(_, _) | TheoryState::Sat) {
+                    break;
+                }
             }
 
-            // If after propagation every theory was satisfied then we have a model
-            if satisfied == (true, true) {
-            	return Answer::Sat;
+            // Assert: Every theory is fully coherent with the trail
+            // Assert: Theory states are necessarily either decision or sat
+            // Assert: Every theory is sat => formula is sat
+            match states {
+                TheoryState::Sat => return Answer::Sat,
+                TheoryState::Decision(t, v) => trail.add_decision(t, v),
+                _ => unreachable!(),
             }
-
-            // Make a decision
-            self.bool_th.decide(trail);
         }
     }
 
-    pub fn resolve_conflict(self, trail: &mut Trail, conflict: Vec<(Term, Value)>) {
+    // Requires `trail` and `conflict` to form a conflict state
+    // Requires that `trail` level is > 0
+    // Ensures that `trail` is non-conflicting
+    pub fn resolve_conflict(&mut self, trail: &mut Trail, conflict: Vec<Assignment>) {
+        // Can store index in trail in as part of the priority using lexicographic order
+        let mut heap : PriorityQueue<Assignment, usize> = PriorityQueue::new();
 
+        // calculate level of the conflict
+        for a in conflict.into_iter() {
+            let l = a.level();
+            heap.push(a, l);
+        }
+
+        let level = *heap.peek().unwrap().1;
+
+        // #[variant()]
+        while let Some((a, l)) = heap.pop() {
+            // back jump
+            if a.is_bool() && l > *heap.peek().unwrap().1 {
+                trail.restrict(*heap.peek().unwrap().1);
+                trail.add_justified(heap.into_vec(), a.term(), a.value().negate());
+                return;
+            };
+
+            if let Some(just) = trail.justification(a) {
+                for j in just.iter() {
+                    if j.level() == level && j.first_order() && j.decision() {
+                        // undo decide
+                        if *heap.peek().unwrap().1 == l {
+                            trail.restrict(level - 1);
+                            trail.add_decision(a.term(), a.value().negate());
+                            return;
+                        }
+                        // undo clear
+                        else {
+                            trail.restrict(level-1);
+                            return;
+                        }
+                    }
+                }
+
+                // resolve
+                for a in j.into_iter() {
+                    let l = a.level();
+                    heap.push(a, l);
+                }
+            }
+        }
     }
 }
 
@@ -41,32 +122,88 @@ pub enum Answer {
 }
 
 enum ExtendResult {
-    Conflict(Vec<(Term, Value)>),
-    Deduction,
+    Conflict(Vec<Assignment>),
+    Decision(Term, Value),
     Satisfied,
-    Saturated,
 }
 
 struct BoolTheory;
+
+// trait Theory {
+//     // Requires `self` to be coherent up to `last_index` with `tl`
+//     // Ensures `self` is fully coherent with `^tl`
+//     // Returns an acceptable decision
+//     // Returns sat when is satisfied by trail
+//     // Returns conflict when there is a conflict
+//     fn extend(&mut self, tl: &mut Trail) -> ExtendResult;
+
+//     // Restricts it's model to be at most `level` deep.
+//     // Ensures that is consistent up to `tl` len
+//     fn restrict(&mut self, level: usize, tl: &Trail);
+
+//     // Must be invariant to extensions of the trail beyond `ix` ie has 'prefix property'
+//     #[predicate]
+//     fn coherent(self, tl: Trail, ix: usize);
+
+//     // The last index that we have seen and can be held accountable for
+//     #[logic]
+//     fn last_index(self) -> usize;
+// }
 
 impl BoolTheory {
     // Extend the trail with 1 or more deductions, or backtrack to a non-conflicting state
     // Returns `Fail` if we encounter a conflict at level 0
     // Return Satisfied if the trail is satisfactory to us
-    fn extend(&self, tl: &mut Trail) -> ExtendResult {
+    fn extend(&mut self, tl: &mut Trail) -> ExtendResult {
         let i = 0;
 
-        let mut res = ExtendResult::Satisfied;
         while i < tl.len() {
         	if tl[i].is_bool() {
-
+                match self.eval(tl, &tl[i].term) {
+                    Result::Err(mut fvs) => {
+                        let decision = fvs.pop().unwrap();
+                        return ExtendResult::Decision(decision, Value::Bool(true))
+                    },
+                    Result::Ok((mut subterms, res)) => {
+                        if res != tl[i].val {
+                            subterms.push(tl[i].clone());
+                            return ExtendResult::Conflict(subterms)
+                        }
+                    }
+                }
         	}
         }
 
-        res
+        return ExtendResult::Satisfied
     }
 
-    fn decide(&self, tl: &mut Trail) {
+    // Ensures:
+    //  - Free Var list is non-empty, all not on trail
+    //  - If ok: there is a justified entailment between the justification and tm <- value?
+    #[ensures(forall<just : _, val: _> result == Ok((just, val)) ==> forall<m : _> m.satisfy_set(@just) ==> m.satisfies((@tm, @val)))]
+    fn eval(&mut self, tl: &Trail, tm: &Term) -> Result<(Vec<Assignment>, Value), Vec<Term>> {
+        match tm {
+            Term::Conj(l, r) => {
+                let l = self.eval(tl, l);
+                let r = self.eval(tl, r);
 
+                match (l, r) {
+                    (Ok((mut j1, v1)), Ok((j2, v2))) => {
+                        j1.extend(j2);
+                        if v1.bool() && v2.bool() {
+                            return Ok((j1, Value::Bool(true)))
+                        } else {
+                            return Ok((j1, Value::Bool(false)))
+                        }
+                    },
+                    (Err(mut f1), Err(f2)) => { f1.extend(f2); return Err(f1)},
+                    (_, Err(f)) | (Err(f), _) => return Err(f)
+                }
+            }
+            a => match tl.get(a) {
+                Some(asn) => Ok((vec![asn.clone()], asn.value().clone())),
+                None => Err(vec![a.clone()]),
+            }
+        }
     }
 }
--- src/trail.rs
+++ src/trail.rs
@@ -3,49 +3,108 @@ use std::ops::Index;
 use num_rational::BigRational;
 
 // Todo: Distinguish between boolean and fo assignments as an optim?
+#[derive(PartialEq, Eq, Hash, Clone)]
 pub struct Assignment {
-    term: Term,
-    val: Value,
+    pub term: Term,
+    pub val: Value,
     reason: Reason,
+    level: usize,
 }
 
+#[derive(PartialEq, Eq, Hash, Clone)]
 enum Reason {
-    Justified,
+    Justified(Vec<usize>),
     Decision,
 }
 
+#[derive(Clone, PartialEq, Eq, Hash)]
 pub enum Term {
     Variable(String),
     Value(Value),
     Plus(Box<Term>, Box<Term>),
     Eq(Box<Term>, Box<Term>),
+    Conj(Box<Term>, Box<Term>),
     // TODO: complete others
 }
 
+#[derive(Clone, PartialEq, Eq, Hash)]
 pub enum Value {
     Bool(bool),
     Rat(BigRational),
 }
 
-pub struct Trail(Vec<Assignment>);
+impl Value {
+    pub fn bool(&self) -> bool {
+        match self {
+            Value::Bool(b) => *b,
+            Value::Rat(_) => unreachable!(),
+        }
+    }
+
+    fn is_bool(&self) -> bool {
+        match self {
+            Value::Bool(_) => true,
+            Value::Rat(_) => false,
+        }
+    }
+
+    pub(crate) fn negate(&self) -> _ {
+        todo!()
+    }
+}
+
+pub struct Trail {
+    assignments: Vec<Assignment>,
+    level: usize,
+}
 
 impl Trail {
     pub fn len(&self) -> usize {
-        self.0.len()
-    }
-
-    // Checks if an assignment is unit and if so returns the term which would make it unit
-    pub(crate) fn is_unit(&self, i: usize) -> Option<&Term> {
-        todo!()
+        self.assignments.len()
     }
 
     // Add a justified assignment to the trail
-    pub(crate) fn add_justified(&self, lit: &Term, polarity: Value) {
+    // pub(crate) fn add_justified(&self, lit: &Term, polarity: Value) {
+    //     todo!()
+    // }
+
+    // pub(crate) fn restrict(&mut self, level: usize) {
+    //     todo!()
+    // }
+
+    pub(crate) fn level(&self) -> usize {
+        self.level
+    }
+
+    pub(crate) fn add_decision(&mut self, term: Term, val: Value) {
+        self.level += 1;
+        self.assignments.push(Assignment {
+            term, val, reason: Reason::Decision, level: self.level,
+        });
+    }
+
+    pub(crate) fn get(&self, a: &Term) -> Option<&Assignment> {
+        let mut i = 0;
+        while i < self.len() {
+            if &self[i].term == a {
+                return Some(&self[i])
+            }
+            i += 1
+        }
+
+        return None
+    }
+
+    pub(crate) fn justification(&self, a: Assignment) -> Option<Vec<Assignment>> {
         todo!()
     }
 
-    pub(crate) fn restrict(&mut self, level: usize) {
-    	todo!()
+    pub(crate) fn add_justified(&self, into_vec: Vec<Assignment>, term: _, negate: _) -> _ {
+        todo!()
+    }
+
+    pub(crate) fn restrict(&self, arg1: usize) -> _ {
+        todo!()
     }
 }
 
@@ -53,19 +112,32 @@ impl Index<usize> for Trail {
     type Output = Assignment;
 
     fn index(&self, index: usize) -> &Self::Output {
-        &self.0[index]
+        &self.assignments[index]
     }
 }
 
-
-impl Term {
-	// Checks if a term is of sort `bool`
-    pub(crate) fn is_bool(&self) -> bool {
-        todo!()
+impl Assignment {
+    pub(crate) fn level(&self) -> usize {
+        self.level
     }
 
-    // Returns the polarity of a *boolean* term
-    pub(crate) fn polarity(&self) -> bool {
-        todo!()
+    pub(crate) fn is_bool(&self) -> bool {
+        self.val.is_bool()
+    }
+
+    pub(crate) fn first_order(&self) -> bool {
+        !self.is_bool()
+    }
+
+    pub(crate) fn decision(&self) -> bool {
+        self.reason == Reason::Decision
+    }
+
+    pub(crate) fn value(&self) -> &Value {
+        &self.val
+    }
+
+    pub(crate) fn term(&self) -> &Term {
+        &self.term
     }
 }
